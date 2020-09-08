80ec9475a2a89ce9859d37ad4289fb6e665a2ec9
clark2668 <baclark@msu.edu>
2020-09-08 14:50:03 -0500

    make all of the step2 scripts for first fully working test

---
+++ step2/make_step2dag.py
@@ -0,0 +1,53 @@
+import numpy as np
+import os
+
+from NuRadioReco.utilities import units
+
+coszenbins = np.linspace(-1, 1, 21)
+logEs = np.arange(15., 20.1, 0.5)
+energies = 10 ** logEs * units.eV
+
+n_parts = 2
+n_parts_per_file = np.ones(len(coszenbins)-1, dtype=np.int) * 1
+flavors = ["e", "mu", "tau"]
+
+step1dir = "/data/user/brianclark/Gen2/simulation_input/secondaries_500km2/step1/"
+step2dir = "/data/user/brianclark/Gen2/simulation_output/secondaries_500km2"
+
+det_file = "dipoles_100m"
+config_file = "config_Alv2009_nonoise_100ns"
+sim_file = "D02single_dipole"
+
+dag_file_name='dagman_step2.dag'
+
+# write the master variables
+instructions = ""
+instructions += 'CONFIG config.dagman\n'
+instructions += f'VARS ALL_NODES step1dir="{step1dir}" step2dir="{step2dir}" detfile="{det_file}" configfile="{config_file}" simfile="{sim_file}"\n\n'
+with open(dag_file_name, 'w') as f:
+	f.write(instructions)
+
+master_index=0
+for flavor in flavors:
+	for iE in range(len(logEs)):
+		if(logEs[iE]<20.0):
+			continue
+
+		for iC in range(len(coszenbins)-1):
+			czen1 = coszenbins[iC]
+			czen2 = coszenbins[iC + 1]
+			if(czen2 > .3 or czen1 < -0.3):
+				continue
+			E = energies[iE]
+			
+			for ijob in range(n_parts//n_parts_per_file[iC]):
+				for ipart in range(ijob * n_parts_per_file[iC], (ijob + 1) * n_parts_per_file[iC]):
+					instructions = ""
+					instructions += f'JOB job_{master_index} step2_job.sub \n'
+					instructions += f'VARS job_{master_index} flavor="{flavor}" energy="{logEs[iE]:.2f}" czmin="{czen1:.1f}" czmax="{czen2:.1f}" part="{ipart:06}"\n\n'
+
+					with open(dag_file_name, 'a') as f:
+						f.write(instructions)
+
+					master_index+=1
+
---
+++ step2/step2_script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# first, load variables
+step1dir=$1
+step2dir=$2
+det_file=$3
+config_file=$4
+sim_file=$5
+flavor=$6
+energy=$7
+czmin=$8
+czmax=$9
+part=${10}
+
+
+# step1dir is where the step1 files ("the input files themselves") will go
+# step2dir is where the step2 files ("actual simulation output") will go
+
+# the string holding the flavor, energy, and cos(zenith) bin information
+meta_info=${flavor}_${energy}eV_${czmin}_${czmax}
+inputfile=${meta_info}.${part}.hdf5
+outputfile=out_${meta_info}.${part}.hdf5
+base_support_dir=/data/user/brianclark/Gen2/simulation_input/support_files
+
+# just so we can have access to gridftp
+eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/setup.sh`
+
+# get the input file
+globus-url-copy gsiftp://gridftp.icecube.wisc.edu/${step1dir}/${flavor}/${meta_info}/${inputfile} ./
+
+# copy in the relevant detector, config, and simulation files
+globus-url-copy gsiftp://gridftp.icecube.wisc.edu/${base_support_dir}/${det_file}.json ./
+globus-url-copy gsiftp://gridftp.icecube.wisc.edu/${base_support_dir}/${config_file}.yaml ./
+globus-url-copy gsiftp://gridftp.icecube.wisc.edu/${base_support_dir}/${sim_file}.py ./
+
+# clear out the python path and source the setup file for gen2 radio simulations
+unset PYTHONPATH
+source /cvmfs/icecube.opensciencegrid.org/users/brianclark/gen2radiosim/setup.sh
+
+# run the python script
+python ${sim_file}.py ${inputfile} ${det_file}.json ${config_file}.yaml ${outputfile}
+
+# bring the results back to the data-warehouse
+globus-url-copy ./out_*hdf5 gsiftp://gridftp.icecube.wisc.edu/${step2dir}/${flavor}/${meta_info}/

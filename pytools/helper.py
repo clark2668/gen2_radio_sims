ee55ae6b9432cc98f5ffe58d74ea3725c929a171
clark2668 <baclark@msu.edu>
2020-09-09 13:08:53 -0500

    update the helper, and make some major renovations to the way number of events and number of parts are handled at step0

--- pytools/helper.py
+++ pytools/helper.py
@@ -6,3 +6,27 @@ def get_coszenbins():
 
 def get_logEs():
 	return np.arange(15., 20.1, 0.5)
+
+def get_number_of_parts_and_events(flavor, logE):
+	num_parts=1
+	num_events=1
+	splitter=4
+
+	
+	if flavor=='e':
+		# the electron flavor is always fast, and can always take 1000 events
+		num_parts=int(100/splitter)
+		num_events=1000
+	elif flavor=='mu' or flavor=='tau':
+		# otherwise, we need to be clever about energy bins
+		if logE<18:
+			num_parts=int(100/splitter)
+			num_events=1000
+		elif logE>=18 and logE<19:
+			num_parts=int(400/splitter)
+			num_events=250
+		elif logE>=19:
+			num_parts=int(2000/splitter)
+			num_events=50
+
+	return num_parts, num_events
--- step0/make_input_configs.py
+++ step0/make_input_configs.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 
 from NuRadioReco.utilities import units
+import helper as hp
 
 base_dir = "/data/user/brianclark/Gen2/simulation_input/"
 
@@ -13,8 +14,8 @@ step1dir = os.path.join(base_dir, f"secondaries_500km2", "step1")
 if(not os.path.exists(step1dir)):
 	os.makedirs(step1dir)
 
-coszenbins = np.linspace(-1, 1, 21)
-logEs = np.arange(15., 20.1, 0.5)
+coszenbins = hp.get_coszenbins()
+logEs = hp.get_logEs()
 energies = 10 ** logEs * units.eV
 
 phimin = 0.*units.deg
@@ -31,17 +32,10 @@ volume = {'fiducial_xmin': -10 * units.km - dX,
 		'full_zmax': 0
 		}
 
-def get_number_of_events(logE):
-	return 1e1
-
 distance_cut_polynomial = np.polynomial.polynomial.Polynomial([-1.56610502e+02, 2.54131322e+01, -1.34932379e+00, 2.39984185e-02])
 
 def get_distance_cut(shower_energy):
 	return max(100 * units.m, 10 ** distance_cut_polynomial(np.log10(shower_energy)))
-
-n_parts = 2
-
-n_parts_per_file = np.ones(len(coszenbins)-1, dtype=np.int) * 1
 
 flavors = ["e", "mu", "tau"]
 
@@ -51,10 +45,9 @@ flavor_ids = {'e': [12,-12],
 
 for flavor in flavors:
 	for iE in range(len(logEs)):
-		#if(logEs[iE]<20.0):
-		#	continue
-		# print (logEs[iE])
-		nevt = get_number_of_events(logEs[iE])
+
+		num_parts, num_events = hp.get_number_of_parts_and_events(flavor, logEs[iE])
+
 		max_dist = get_distance_cut(10 ** logEs[iE])
 		print(f"maximum radius for E = {10**logEs[iE]:.2g} is {max_dist/units.m:.0f}m")
 
@@ -64,13 +57,12 @@ for flavor in flavors:
 		for iC in range(len(coszenbins) - 1):
 			czen1 = coszenbins[iC]
 			czen2 = coszenbins[iC + 1]
-			if(czen2 > .3 or czen1 < -0.3):
-				continue
 			E = energies[iE]
 			thetamax = np.arccos(czen1)
 			thetamin = np.arccos(czen2)
 			pattern = f"{flavor}_{logEs[iE]:.2f}eV_{czen1:.1f}_{czen2:.1f}"
 			print(pattern)
+			
 			folder = os.path.join(step0dir, flavor, f"{pattern}")
 			if(not os.path.exists(folder)):
 				os.makedirs(folder)
@@ -79,23 +71,21 @@ for flavor in flavors:
 			if(not os.path.exists(folder1)):
 				os.makedirs(folder1)
 
-			for ijob in range(n_parts//n_parts_per_file[iC]):
+			for ijob in range(num_parts):
 				
 				instruction = ""
 				instruction += 'from NuRadioMC.EvtGen.generator import generate_eventlist_cylinder\n'
 				instruction += 'from NuRadioReco.utilities import units\n\n'
-				for ipart in range(ijob * n_parts_per_file[iC], (ijob + 1) * n_parts_per_file[iC]):
-					out_filename = "in_" + f"{pattern}" + f".part{ipart:06}" + ".hdf5"
-					start_event_id = int(ipart * nevt) + 1
-					instruction += f"generate_eventlist_cylinder('{out_filename}', {nevt}, {E}, {E}, {volume},\n"
-					instruction += f"thetamin={thetamin}, thetamax={thetamax}, phimin={phimin},\n"
-					instruction += f"phimax={phimax}, \n"
-					instruction += f"start_event_id={start_event_id},\n"
-					instruction += f"proposal=True, proposal_config='config_PROPOSAL.json', n_events_per_file=None,\n"
-					instruction += f"flavor={flavor_ids[flavor]},\n"
-					instruction += f"proposal_kwargs={{'low_nu': 1 * units.PeV, 'min_energy_loss_nu': 1 * units.PeV}})\n"
-					instruction += "\n"
-				
+				out_filename = "in_" + f"{pattern}" + f".part{ijob:06}" + ".hdf5"
+				start_event_id = int(ijob * num_events) + 1
+				instruction += f"generate_eventlist_cylinder('{out_filename}', {num_events}, {E}, {E}, {volume},\n"
+				instruction += f"thetamin={thetamin}, thetamax={thetamax}, phimin={phimin},\n"
+				instruction += f"phimax={phimax}, \n"
+				instruction += f"start_event_id={start_event_id},\n"
+				instruction += f"proposal=True, proposal_config='config_PROPOSAL.json', n_events_per_file=None,\n"
+				instruction += f"flavor={flavor_ids[flavor]},\n"
+				instruction += f"proposal_kwargs={{'low_nu': 1 * units.PeV, 'min_energy_loss_nu': 1 * units.PeV}})\n"
+				instruction += "\n"
 				python_filename = f'{pattern}_{ijob:06d}.py'
 				
 				if(not os.path.exists(os.path.join(step0dir, flavor, pattern))):

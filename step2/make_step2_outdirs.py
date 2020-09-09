cfdcb8c323f58b26b344c9f01a0c5be95864aec9
clark2668 <baclark@msu.edu>
2020-09-09 14:53:39 -0500

    rework step2 make scripts to utilize the helper utility class

--- step2/make_step2_outdirs.py
+++ step2/make_step2_outdirs.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 
 from NuRadioReco.utilities import units
+import helper as hp
 
 base_dir = "/data/user/brianclark/Gen2/simulation_output/"
 
@@ -13,34 +14,20 @@ step2dir = os.path.join(base_dir, f"secondaries_500km2", f"{det_file}", f"{confi
 if(not os.path.exists(step2dir)):
 	os.makedirs(step2dir)
 
-coszenbins = np.linspace(-1, 1, 21)
-logEs = np.arange(15., 20.1, 0.5)
+coszenbins = hp.get_coszenbins()
+logEs = hp.get_logEs()
 energies = 10 ** logEs * units.eV
-
-n_parts = 2
-
-n_parts_per_file = np.ones(len(coszenbins)-1, dtype=np.int) * 1
 
 flavors = ["e", "mu", "tau"]
 
 for flavor in flavors:
 	for iE in range(len(logEs)):
-		#if(logEs[iE]<20.0):
-		#	continue
-
 		for iC in range(len(coszenbins) - 1):
 			czen1 = coszenbins[iC]
 			czen2 = coszenbins[iC + 1]
-			if(czen2 > .3 or czen1 < -0.3):
-				continue
 			E = energies[iE]
 			pattern = f"{flavor}_{logEs[iE]:.2f}eV_{czen1:.1f}_{czen2:.1f}"
 			print(pattern)
 			folder = os.path.join(step2dir, flavor, f"{pattern}")
 			if(not os.path.exists(folder)):
 				os.makedirs(folder)
-
-			for ijob in range(n_parts//n_parts_per_file[iC]):
-						
-				if(not os.path.exists(os.path.join(step2dir, flavor, pattern))):
-					os.makedirs(os.path.join(step2dir, flavor, pattern))

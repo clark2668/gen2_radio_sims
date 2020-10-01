334c2eed676b4ece96456feb9ad8087dc4ab44fc
clark2668 <baclark@msu.edu>
2020-10-01 10:50:34 -0500

    enable us to runo ver many detector files at once

--- scripts/step2/make_step2_dag.py
+++ scripts/step2/make_step2_dag.py
@@ -13,37 +13,45 @@ flavors = ["e", "mu", "tau"]
 step1dir = "/data/user/brianclark/Gen2/simulation_input/secondaries_500km2/step1/"
 step2dir = "/data/user/brianclark/Gen2/simulation_output/secondaries_500km2"
 
-det_file = "dipoles_100m"
+det_files_dict = {
+	"dipoles_RNOG_200m_2.00km" : "gen2r_200m_2km",
+	"dipoles_RNOG_20m_1.00km" : "gen2r_20m_1km"
+}
+
+det_files_labels = ["dipoles_RNOG_200m_2.00km", "dipoles_RNOG_20m_1.00km"]
 config_file = "config_Alv2009_nonoise_100ns"
-sim_file = "D02single_dipole"
+sim_file = "D02single_dipole_250MHz"
 
-for flavor in flavors:
 
-	dag_file_name='dagman_step2_'+flavor+'.dag'
-	instructions = ""
-	instructions += 'CONFIG config.dagman\n'
-	instructions += f'VARS ALL_NODES step1dir="{step1dir}" step2dir="{step2dir}" detfile="{det_file}" configfile="{config_file}" simfile="{sim_file}"\n\n'
-	with open(dag_file_name, 'w') as f:
-		f.write(instructions)
+for det_file_label in det_files_labels:
+	for flavor in flavors:
 
-	master_index=0
+		det_file = det_files_dict[det_file_label]
+		dag_file_name='dagman_step2_'+det_file+'_'+flavor+'.dag'
+		instructions = ""
+		instructions += 'CONFIG config.dagman\n'
+		instructions += f'VARS ALL_NODES step1dir="{step1dir}" step2dir="{step2dir}" detfile="{det_file}" configfile="{config_file}" simfile="{sim_file}"\n\n'
+		with open(dag_file_name, 'w') as f:
+			f.write(instructions)
 
-	for iE in range(len(logEs)):
+		master_index=0
 
-		num_parts, num_events = hp.get_number_of_parts_and_events(flavor, logEs[iE])
+		for iE in range(len(logEs)):
 
-		for iC in range(len(coszenbins)-1):
-			czen1 = coszenbins[iC]
-			czen2 = coszenbins[iC + 1]
-			E = energies[iE]
-			
-			for ijob in range(num_parts):
-				instructions = ""
-				instructions += f'JOB job_{master_index} step2_job.sub \n'
-				instructions += f'VARS job_{master_index} flavor="{flavor}" energy="{logEs[iE]:.2f}" czmin="{czen1:.1f}" czmax="{czen2:.1f}" part="{ijob:06}"\n\n'
+			num_parts, num_events = hp.get_number_of_parts_and_events(flavor, logEs[iE])
 
-				with open(dag_file_name, 'a') as f:
-					f.write(instructions)
+			for iC in range(len(coszenbins)-1):
+				czen1 = coszenbins[iC]
+				czen2 = coszenbins[iC + 1]
+				E = energies[iE]
+				
+				for ijob in range(num_parts):
+					instructions = ""
+					instructions += f'JOB job_{master_index} step2_job.sub \n'
+					instructions += f'VARS job_{master_index} flavor="{flavor}" energy="{logEs[iE]:.2f}" czmin="{czen1:.1f}" czmax="{czen2:.1f}" part="{ijob:06}"\n\n'
 
-				master_index+=1
+					with open(dag_file_name, 'a') as f:
+						f.write(instructions)
+
+					master_index+=1
 
--- scripts/step2/make_step2_outdirs.py
+++ scripts/step2/make_step2_outdirs.py
@@ -6,28 +6,29 @@ import helper as hp
 
 base_dir = "/data/user/brianclark/Gen2/simulation_output/"
 
-det_file = "dipoles_100m"
+det_files = ["dipoles_RNOG_200m_2.00km", "dipoles_RNOG_20m_1.00km"]
 config_file = "config_Alv2009_nonoise_100ns"
-sim_file = "D02single_dipole"
+sim_file = "D02single_dipole_250MHz"
 
-step2dir = os.path.join(base_dir, f"secondaries_500km2", f"{det_file}", f"{config_file}", f"{sim_file}")
-if(not os.path.exists(step2dir)):
-	os.makedirs(step2dir)
+for det_file in det_files:
+	step2dir = os.path.join(base_dir, f"secondaries_500km2", f"{det_file}", f"{config_file}", f"{sim_file}")
+	if(not os.path.exists(step2dir)):
+		os.makedirs(step2dir)
 
-coszenbins = hp.get_coszenbins()
-logEs = hp.get_logEs()
-energies = 10 ** logEs * units.eV
+	coszenbins = hp.get_coszenbins()
+	logEs = hp.get_logEs()
+	energies = 10 ** logEs * units.eV
 
-flavors = ["e", "mu", "tau"]
+	flavors = ["e", "mu", "tau"]
 
-for flavor in flavors:
-	for iE in range(len(logEs)):
-		for iC in range(len(coszenbins) - 1):
-			czen1 = coszenbins[iC]
-			czen2 = coszenbins[iC + 1]
-			E = energies[iE]
-			pattern = f"{flavor}_{logEs[iE]:.2f}eV_{czen1:.1f}_{czen2:.1f}"
-			print(pattern)
-			folder = os.path.join(step2dir, flavor, f"{pattern}")
-			if(not os.path.exists(folder)):
-				os.makedirs(folder)
+	for flavor in flavors:
+		for iE in range(len(logEs)):
+			for iC in range(len(coszenbins) - 1):
+				czen1 = coszenbins[iC]
+				czen2 = coszenbins[iC + 1]
+				E = energies[iE]
+				pattern = f"{flavor}_{logEs[iE]:.2f}eV_{czen1:.1f}_{czen2:.1f}"
+				print(pattern)
+				folder = os.path.join(step2dir, flavor, f"{pattern}")
+				if(not os.path.exists(folder)):
+					os.makedirs(folder)

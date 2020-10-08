96b06049a77c9e480b5cbd3d602623e1f624f5b3
clark2668 <baclark@msu.edu>
2020-10-08 15:25:13 -0500

    tweak step 3 further to allow it to run over several different output files

--- scripts/step3/make_step3_dag.py
+++ scripts/step3/make_step3_dag.py
@@ -1,27 +1,29 @@
 flavors = ["e", "mu", "tau"]
+flavors = ["e"]
 
 step2dir = "/data/user/brianclark/Gen2/simulation_output/secondaries_500km2"
 
-det_file = "dipoles_100m"
+det_files = ["dipoles_RNOG_200m_2.00km", "dipoles_RNOG_20m_1.00km"]
 config_file = "config_Alv2009_nonoise_100ns"
-sim_file = "D02single_dipole"
+sim_file = "D02single_dipole_250MHz"
 
 
 dag_file_name='dagman_step3.dag'
 instructions = ""
 instructions += 'CONFIG config.dagman\n'
-instructions += f'VARS ALL_NODES step2dir="{step2dir}/{det_file}/{config_file}/{sim_file}/"\n\n'
 with open(dag_file_name, 'w') as f:
 	f.write(instructions)
 
 master_index=0
-for flavor in flavors:
+for det_file in det_files:
 
-	instructions = ""
-	instructions += f'JOB job_{master_index} step3_job.sub \n'
-	instructions += f'VARS job_{master_index} flavor="{flavor}" \n\n'
+	for flavor in flavors:
 
-	with open(dag_file_name, 'a') as f:
-		f.write(instructions)
+		instructions = ""
+		instructions += f'JOB job_{master_index} step3_job.sub \n'
+		instructions += f'VARS job_{master_index} flavor="{flavor}" step2dir="{step2dir}/{det_file}/{config_file}/{sim_file}/"\n\n'
 
-	master_index+=1
+		with open(dag_file_name, 'a') as f:
+			f.write(instructions)
+
+		master_index+=1

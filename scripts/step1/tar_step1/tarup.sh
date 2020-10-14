f48aca03b6af84348859678108e0844fc290d56b
clark2668 <baclark@msu.edu>
2020-10-14 13:58:52 -0500

    add two little scripts for individual tarring

---
+++ scripts/step1/tar_step1/tarup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+declare -a flavors=("tau")
+
+for flavor in "${flavors[@]}"
+do
+	dirlist=${flavor}_*
+	echo $dirlist
+	for dir in $dirlist
+	do
+		cd $dir
+		files=*.hdf5
+		
+		for file in $files
+		do
+			echo $file
+			tar -czvf ${file}.tar.gz $file
+			rm $file
+		done
+		cd ..
+	done
+done

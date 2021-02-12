a9b7e6db0d698f4acf33c64cd637457a0353b2b3
Brian Clark <baclark@msu.edu>
2021-02-11 20:23:55 -0500

    make sure to count the stations right

--- analysis/2021.02_review/plot_limit/plot_limit.py
+++ analysis/2021.02_review/plot_limit/plot_limit.py
@@ -6,8 +6,9 @@ from NuRadioReco.utilities import units
 
 
 n_deep = 144
-n_shallow = 169
+n_shallow = (169+144)
 livetime = 10 * units.year
+uptime = 1.
 
 data = np.genfromtxt('tabulated_veff_aeff.csv', delimiter=',', skip_header=1, names=['logE', 'dveff', 'daff', 'sveff', 'saeff'])
 energies = np.power(10.,data['logE'])
@@ -22,13 +23,13 @@ fig, ax = limits.get_E2_limit_figure(show_ice_cube_EHE_limit=True,
 										diffuse=True, show_grand_10k=False, show_grand_200k=False, show_Heinze=True, show_TA=True,
 										show_ara=False, show_arianna=False, show_IceCubeGen2=False, shower_Auger=False)
 
-limit_deep = fluxes.get_limit_e2_flux(energy=energies[1:]*units.eV, veff_sr= n_deep * data['dveff'][1:]*units.km**3*units.sr, livetime=livetime)
-limit_shallow = fluxes.get_limit_e2_flux(energy=energies[1:]*units.eV, veff_sr= n_shallow * data['sveff'][1:]*units.km**3*units.sr, livetime=livetime)
-limit_combo = fluxes.get_limit_e2_flux(energy=energies[1:]*units.eV, veff_sr= (n_shallow * data['sveff'][1:]*units.km**3*units.sr) + (n_deep * data['dveff'][1:]*units.km**3*units.sr), livetime=livetime)
+limit_deep = fluxes.get_limit_e2_flux(energy=energies[1:]*units.eV, veff_sr= n_deep * data['dveff'][1:]*units.km**3*units.sr, livetime=livetime*uptime)
+limit_shallow = fluxes.get_limit_e2_flux(energy=energies[1:]*units.eV, veff_sr= n_shallow * data['sveff'][1:]*units.km**3*units.sr, livetime=livetime*uptime)
+limit_combo = fluxes.get_limit_e2_flux(energy=energies[1:]*units.eV, veff_sr= (n_shallow * data['sveff'][1:]*units.km**3*units.sr) + (n_deep * data['dveff'][1:]*units.km**3*units.sr), livetime=livetime*uptime)
 
-ax.plot(np.power(10.,data['logE'][1:])/limits.plotUnitsEnergy, limit_deep/limits.plotUnitsFlux, 'C0--', label='Deep Only ({} sta, {} yrs)'.format(n_deep , livetime/units.year))
-ax.plot(np.power(10.,data['logE'][1:])/limits.plotUnitsEnergy, limit_shallow/limits.plotUnitsFlux, 'C1--', label='Shallow Only ({} sta, {} yrs)'.format(n_shallow , livetime/units.year))
-ax.plot(np.power(10.,data['logE'][1:])/limits.plotUnitsEnergy, limit_combo/limits.plotUnitsFlux, 'C2--', label='Combo ({} deep, {} shallow, {} yrs)'.format(n_deep, n_shallow , livetime/units.year))
+ax.plot(np.power(10.,data['logE'][1:])/limits.plotUnitsEnergy, limit_shallow/limits.plotUnitsFlux, 'C1--', label='Shallow Only ({} sta, {} yrs, {}% uptime)'.format(n_shallow , livetime/units.year, uptime*100))
+ax.plot(np.power(10.,data['logE'][1:])/limits.plotUnitsEnergy, limit_deep/limits.plotUnitsFlux, 'C0--', label='Deep Only ({} sta, {} yrs, {}% uptime)'.format(n_deep , livetime/units.year, uptime*100))
+ax.plot(np.power(10.,data['logE'][1:])/limits.plotUnitsEnergy, limit_combo/limits.plotUnitsFlux, 'C2--', label='Combo ({} deep, {} shallow, {} yrs, {}% uptime)'.format(n_deep, n_shallow , livetime/units.year, uptime*100))
 ax.legend()
 ax.set_title('Trigger Level')
-fig.savefig('test.png')
+fig.savefig('limit_v2.png')

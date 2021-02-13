fix array calculation code so that bins without two files full of events doesn't get skipped incorrectly

analysis/2021.02_review/coincidences/calc_veff_array.py
@@ -38,7 +38,7 @@ for i in range(len(matching_data)):
 
 good_deep = np.genfromtxt('good_deep.csv', delimiter=',', skip_header=0, names=['ID'])
 good_deep = good_deep['ID']
-good_deep = []
+# good_deep = []
 good_shallow = np.genfromtxt('good_shallow.csv', delimiter=',', skip_header=0, names=['ID'])
 good_shallow = good_shallow['ID']
 # good_shallow = []
@@ -60,10 +60,27 @@ def tmp(filename):
 	fin = h5py.File(filename, "r")
 	fin2 = h5py.File(filename2, "r")
 
+	# some summary information we can always return
+	summary = {}
+	summary['n_events'] = fin.attrs['n_events']
+	summary['volume'] = fin.attrs['volume']
+	summary['czmin'] = np.cos(fin.attrs['thetamin'])
+	summary['czmax'] = np.cos(fin.attrs['thetamax'])
+	summary['tot_weight'] = 0 # intialize these to zero
+	summary['deep_only_weight'] = 0 # initialize these to zero
+	summary['shallow_only_weight'] = 0 # initialize these to zero
+	summary['dual_weight'] = 0 # initialize these to zero
+
+	skip_deep = False
+	skip_shallow = False
+
 	if not("event_group_ids" in fin.keys()):
-		return None
+		skip_deep = True
 	if not("event_group_ids" in fin2.keys()):
-		return None
+		skip_shallow=True
+
+	if skip_deep and skip_shallow:
+		return summary
 
 	# our goal is to figure out
 	# first, how many events triggered in either the deep or the surface array
@@ -77,60 +94,77 @@ def tmp(filename):
 
 	# first, get the group ids of the events that triggered either in deep or in shallow
 
-	ugids1, uindex1 = np.unique(np.array(fin['event_group_ids']), return_index=True)
-	ugids2, uindex2 = np.unique(np.array(fin2['event_group_ids']), return_index=True)
-	if(len(ugids1) == 0 or len(ugids2) == 0):
-		return None
+	if not skip_deep:
+		ugids1, uindex1 = np.unique(np.array(fin['event_group_ids']), return_index=True)
+	if not skip_shallow:
+		ugids2, uindex2 = np.unique(np.array(fin2['event_group_ids']), return_index=True)
 
-	weights1 = np.array(fin['weights'])[uindex1]  # this gives us the weight per unique group id
-	weights2 = np.array(fin2['weights'])[uindex2]  # this gives us the weight per unique group id
+	if not skip_deep:
+		if(len(ugids1)==0):
+			skip_deep=True
+	if not skip_shallow:
+		if(len(ugids2)==0):
+			skip_shallow=True
 
-	weights_dict_deep = {A:B for A, B in zip(ugids1, weights1)}
-	weights_dict_shallow = {A:B for A, B in zip(ugids2, weights2)}
+	if skip_deep and skip_shallow:
+		return summary
 
-	tnames = fin.attrs['trigger_names']
-	tname_to_index = {}
-	for i, key in enumerate(tnames):
-		tname_to_index[key] = i
+	if not skip_deep:
+		weights1 = np.array(fin['weights'])[uindex1]  # this gives us the weight per unique group id
+		weights_dict_deep = {A:B for A, B in zip(ugids1, weights1)}
+	if not skip_shallow:
+		weights2 = np.array(fin2['weights'])[uindex2]  # this gives us the weight per unique group id
+		weights_dict_shallow = {A:B for A, B in zip(ugids2, weights2)}
 
-	tnames2 = fin2.attrs['trigger_names']
-	tname_to_index2 = {}
-	for i, key in enumerate(tnames2):
-		tname_to_index2[key] = i
+
+	if not skip_deep:
+		tnames = fin.attrs['trigger_names']
+		tname_to_index = {}
+		for i, key in enumerate(tnames):
+			tname_to_index[key] = i
+
+	if not skip_shallow:
+		tnames2 = fin2.attrs['trigger_names']
+		tname_to_index2 = {}
+		for i, key in enumerate(tnames2):
+			tname_to_index2[key] = i
 
 	# first, the deep station
-	for key in fin.keys():
-		if(key.startswith("station")):
-			deep_id = int(key.split("_")[1]) # get the deep id
-			if deep_id not in good_deep: # skip detectors not in the review array
-				continue
-			s_deep = fin[f'station_{deep_id}']
-			if 'multiple_triggers_per_event' in s_deep:
-				triggers_deep = s_deep['multiple_triggers_per_event'][:, tname_to_index[trigger_names[0]]]
-				evs_triggers_deep = np.unique(np.array(s_deep['event_group_ids'])[triggers_deep])
+	if not skip_deep:
+		for key in fin.keys():
+			if(key.startswith("station")):
+				deep_id = int(key.split("_")[1]) # get the deep id
+				if deep_id not in good_deep: # skip detectors not in the review array
+					continue
+				s_deep = fin[f'station_{deep_id}']
+				if 'multiple_triggers_per_event' in s_deep:
+					triggers_deep = s_deep['multiple_triggers_per_event'][:, tname_to_index[trigger_names[0]]]
+					evs_triggers_deep = np.unique(np.array(s_deep['event_group_ids'])[triggers_deep])
 
-				for ev in evs_triggers_deep:
-					if ev not in event_information:
-						event_information[ev] = [weights_dict_deep[ev], 0, 0]
-						event_information[ev][1] = 1 # mark that it is seen in SOME deep station
-						# (0) store the weight, (1) seen in ANY deep, (2) seen in ANY shallow
+					for ev in evs_triggers_deep:
+						if ev not in event_information:
+							event_information[ev] = [weights_dict_deep[ev], 0, 0]
+							event_information[ev][1] = 1 # mark that it is seen in SOME deep station
+							# (0) store the weight, (1) seen in ANY deep, (2) seen in ANY shallow
 
-	for key in fin2.keys():
-		if(key.startswith("station")):
-			shallow_id = int(key.split("_")[1]) # get the shallow id
-			if shallow_id not in good_shallow: # skip the detectors not in the review array
-				continue
-			s_shallow = fin2[f'station_{shallow_id}']
-			if 'multiple_triggers_per_event' in s_shallow:
-				triggers_shallow = s_shallow['multiple_triggers_per_event'][:, tname_to_index2[trigger_names[0]]]
-				evs_triggers_shallow = np.unique(np.array(s_shallow['event_group_ids'])[triggers_shallow])
+	# then, the shallow
+	if not skip_shallow:
+		for key in fin2.keys():
+			if(key.startswith("station")):
+				shallow_id = int(key.split("_")[1]) # get the shallow id
+				if shallow_id not in good_shallow: # skip the detectors not in the review array
+					continue
+				s_shallow = fin2[f'station_{shallow_id}']
+				if 'multiple_triggers_per_event' in s_shallow:
+					triggers_shallow = s_shallow['multiple_triggers_per_event'][:, tname_to_index2[trigger_names[0]]]
+					evs_triggers_shallow = np.unique(np.array(s_shallow['event_group_ids'])[triggers_shallow])
 
-				for ev in evs_triggers_shallow:
-					if ev not in event_information:
-						event_information[ev] = [weights_dict_shallow[ev], 0, 0]
-						event_information[ev][2] = 1 # mark that it is seen in SOME shallow station
-					elif ev in event_information:
-						event_information[ev][2]=1
+					for ev in evs_triggers_shallow:
+						if ev not in event_information:
+							event_information[ev] = [weights_dict_shallow[ev], 0, 0]
+							event_information[ev][2] = 1 # mark that it is seen in SOME shallow station
+						elif ev in event_information:
+							event_information[ev][2]=1
 
 	tot_weight = 0
 	deep_only_weight = 0
@@ -159,7 +193,6 @@ def tmp(filename):
 	# print("Dual weight {}".format(dual_weight))
 	# print("Sum: {}".format(deep_only_weight + shallow_only_weight + dual_weight))
 
-	summary = {}
 	summary['n_events'] = fin.attrs['n_events']
 	summary['volume'] = fin.attrs['volume']
 	summary['czmin'] = np.cos(fin.attrs['thetamin'])
@@ -209,14 +242,14 @@ for flavor in flavors:
 		total_veff[f"{lgE:.1f}"]['shallow_only_veff'] = gwe(combined_shallow_only_veff/num_zen_bins)
 		total_veff[f"{lgE:.1f}"]['dual_veff'] = gwe(combined_dual_veff/num_zen_bins)
 
-		print("dual veff at 1 EeV for {} is {}".format(total_veff[f"{lgE:.1f}"]['total_veff']/units.km**3 * 4 * np.pi, flavor))
+		print("total veff at 1 EeV for {} is {}".format(total_veff[f"{lgE:.1f}"]['total_veff']/units.km**3 * 4 * np.pi, flavor))
 
-	# # dump this to hdf5 file
-	# pkl_file_name = os.path.join('overlap_' + path.split("/")[-4] + "_" + trigger_names[0] + "_" + path2.split("/")[-4] + "_" + trigger_names2[0] + "_" + flavor + ".pkl")
-	# with open(pkl_file_name, "wb") as fout:
-	# 	pickle.dump(total_veff, fout, protocol=4)
-	
 	# dump this to hdf5 file
-	pkl_file_name = os.path.join('shallow_only_' + flavor + ".pkl")
+	pkl_file_name = os.path.join('overlap_' + path.split("/")[-4] + "_" + trigger_names[0] + "_" + path2.split("/")[-4] + "_" + trigger_names2[0] + "_" + flavor + ".pkl")
 	with open(pkl_file_name, "wb") as fout:
 		pickle.dump(total_veff, fout, protocol=4)
+	
+	# # dump this to hdf5 file
+	# pkl_file_name = os.path.join('shallow_only_' + flavor + ".pkl")
+	# with open(pkl_file_name, "wb") as fout:
+	# 	pickle.dump(total_veff, fout, protocol=4)
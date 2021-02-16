c61ff33bfc0ce99c72ab0fa8197b27a62d901839
Brian Clark <baclark@msu.edu>
2021-02-15 20:41:13 -0500

    update sky coverage plots

--- analysis/2021.02_review/sky_coverage/plot_sky_coverage.py
+++ analysis/2021.02_review/sky_coverage/plot_sky_coverage.py
@@ -4,55 +4,61 @@ import scipy as scipy
 from scipy import interpolate
 
 
-n_deep = 0 #144
-n_shallow = (169+144)
-uptime = 1.
-which_e_bin = 6
+n_deep = 144
+n_shallow = 0#(169+144)
 
 deep_det = 'pa_200m_2.00km'
 shallow_det = 'surface_1.00km'
-filename=f'tabulated_aeff_vs_zen_{deep_det}_{shallow_det}_Ebin_{which_e_bin}.csv'
+filename=f'tabulated_aeff_vs_coszen_review_hybrid.csv'
 
-data = np.genfromtxt(filename, delimiter=',', skip_header=1, names=['czmin', 'daeff', 'saeff'])
+data = np.genfromtxt(filename, delimiter=',', skip_header=6, names=['czmin', 'daeff', 'saeff', 'frac'])
 
-# plot of coverage vs zenith
-fig = plt.figure(figsize=(5,5))
-ax = fig.add_subplot(111)
-ax.plot(data['czmin'], data['daeff']*n_deep*2, label='deep')
-ax.plot(data['czmin'], data['saeff']*n_shallow*2, label='shallow')
-ax.set_xlabel('coz(zen)')
-ax.set_ylabel(r'Aeff $\times 2 \pi$ [$km^2$]')
-ax.set_yscale('log')
-ax.set_ylim([1e-3,10])
-ax.legend()
-ax.set_title('Trigger Level')
-plt.tight_layout()
-fig.savefig('aeff_vs_coszen.png')
-
-
-# zen_for_interp = np.append(data['czmin'], -1)
-# daeff_for_interp = np.append(data['daeff'], data['daeff'][-1])
-# saeff_for_interp = np.append(data['saeff'], data['saeff'][-1])
-
-zen_for_interp = data['czmin']
-zen_for_interp-=0.05
-daeff_for_interp = data['daeff']
-saeff_for_interp = data['saeff']
-
-zen_for_interp = np.flip(zen_for_interp)
-daeff_for_interp = np.flip(daeff_for_interp)
-saeff_for_interp = np.flip(saeff_for_interp)
-
-# tck = interpolate.splrep(zen_for_interp, daeff_for_interp, k=2)
-interpolator_deep = scipy.interpolate.interp1d(zen_for_interp,daeff_for_interp,fill_value='extrapolate')
-interpolator_shallow = scipy.interpolate.interp1d(zen_for_interp,saeff_for_interp,fill_value='extrapolate')
-
-zen_bins_for_digi = data['czmin']
-zen_bins_for_digi = np.append(zen_bins_for_digi,-1.)
-# zen_bin_to_aeff_dict = {A:B for A,B in zip(data['czmin'], data['daeff'])}
+plot_aeff_vs_zen = False
+if plot_aeff_vs_zen:
+	# plot of coverage vs zenith
+	fig = plt.figure(figsize=(5,5))
+	ax = fig.add_subplot(111)
+	ax.plot(data['czmin'], data['daeff']*n_deep, label='deep')
+	ax.plot(data['czmin'], data['saeff']*n_shallow, label='shallow')
+	ax.set_xlabel('coz(zen)')
+	ax.set_ylabel(r'Aeff [$km^2$]')
+	ax.set_yscale('log')
+	ax.set_ylim([1e-3,10])
+	ax.legend()
+	ax.set_title('Trigger Level')
+	plt.tight_layout()
+	fig.savefig('aeff_vs_coszen.png')
 
 doHealpyPlot = True
 if doHealpyPlot:
+
+	# pad a bit to get digitization to work right
+	# zen_for_interp = np.append(data['czmin'], -1)
+	# daeff_for_interp = np.insert(data['daeff'],0, data['daeff'][0])
+	# saeff_for_interp = np.insert(data['saeff'],0, data['saeff'][0])
+	# daeff_for_interp = np.append(data['daeff'], data['daeff'][-1])
+	# saeff_for_interp = np.append(data['saeff'], data['saeff'][-1])
+
+	zen_for_interp = data['czmin']
+	zen_for_interp+=0.05
+	daeff_for_interp = data['daeff']
+	saeff_for_interp = data['saeff']
+	fractions = data['frac']
+
+	zen_for_interp = np.flip(zen_for_interp)
+	daeff_for_interp = np.flip(daeff_for_interp)
+	saeff_for_interp = np.flip(saeff_for_interp)
+	fractions = np.flip(fractions)
+
+	# tck = interpolate.splrep(zen_for_interp, daeff_for_interp, k=2)
+	interpolator_deep = scipy.interpolate.interp1d(zen_for_interp,daeff_for_interp,fill_value='extrapolate')
+	interpolator_shallow = scipy.interpolate.interp1d(zen_for_interp,saeff_for_interp,fill_value='extrapolate')
+	interpolator_fraction = scipy.interpolate.interp1d(zen_for_interp, fractions,fill_value='extrapolate')
+
+	# zen_bins_for_digi = data['czmin']
+	# zen_bins_for_digi = np.insert(zen_bins_for_digi, 0, 1)
+	# zen_bins_for_digi = np.append(zen_bins_for_digi,-1.)
+	# zen_bin_to_aeff_dict = {A:B for A,B in zip(data['czmin'], data['daeff'])}
 
 	import healpy as hlpy
 	nside = 64
@@ -63,10 +69,11 @@ if doHealpyPlot:
 	for ipix in range(npix):
 		theta, phi = hlpy.pix2ang(nside, ipix) # where it's coming from
 		cos_theta = -1 * np.cos(theta)
-		thebin = np.digitize(cos_theta, zen_bins_for_digi)
+		# thebin = np.digitize(cos_theta, zen_bins_for_digi)
 		# aeff_this  = data['daeff'][thebin-1]*n_deep + data['saeff'][thebin-1]*n_shallow
 		# aeff_this = interpolate.splev(cos_theta, tck)
-		aeff_this = interpolator_deep(cos_theta)*n_deep + interpolator_shallow(cos_theta)*n_shallow
+		aeff_this = (interpolator_deep(cos_theta)*n_deep + interpolator_shallow(cos_theta)*n_shallow)*(1-interpolator_fraction(cos_theta))
+		# aeff_this = interpolator_fraction(cos_theta)
 		if cos_theta > -2:
 			# scan[ipix] = np.log10(aeff_this)
 			# if(aeff_this<1E-4):
@@ -94,7 +101,7 @@ if doHealpyPlot:
 							unit='\n'+r'$A_{eff} \,\,[km^2]$',
 							# unit='\n'+r'Relative Effective Area',)
 							min=0,
-							max=0.4,
+							max=0.36,
 							cmap='Reds')
 	hlpy.graticule()
 	fig.savefig('sky_coverage_{}deep_{}shallow.png'.format(n_deep, n_shallow), dpi=300)

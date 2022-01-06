c4a78651cd5750250fa9ac50f2ac556d77c9c00f
clark2668 <baclark@msu.edu>
2022-01-06 16:56:44 -0600

    switch over enviroment variables to share the helper function with the main analysis scripts

--- env.sh
+++ env.sh
@@ -1,2 +1,3 @@
 pwd=$PWD
-export PYTHONPATH=$pwd/pytools:$PYTHONPATH
+#export PYTHONPATH=$pwd/pytools:$PYTHONPATH
+export PYTHONPATH=/home/brianclark/Gen2/radio/analysis-scripts/gen2-tdr-2021/production_scripts:$PYTHONPATH

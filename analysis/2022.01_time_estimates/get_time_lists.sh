082b5219b680be18be497cb4ed41da8021ad423c
clark2668 <baclark@msu.edu>
2022-01-09 17:02:11 -0600

    rename the get times script, and add a plotting script

---
+++ analysis/2022.01_time_estimates/get_time_lists.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# declare -a dets=("baseline_array" "hex_hybrid_only_array" "hex_shallow_array" "hex_shallowheavy_array")
+declare -a dets=("baseline_array" "hex_hybrid_only_array")
+
+declare -a flavs=("e" "mu" "tau")
+
+head_dir="/scratch/brianclark/gen2radiosims/trash/step2/"
+
+for det in "${dets[@]}"
+do
+    echo $det
+    for flav in "${flavs[@]}"
+    do
+        echo $flav
+        grep "events processed" $head_dir/$det/$flav/err/* >> times_${det}_${flav}.txt
+    done
+done

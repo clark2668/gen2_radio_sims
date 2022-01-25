12e962e665c3411976077dfe88eae6de8c636cfc
clark2668 <baclark@msu.edu>
2022-01-25 15:58:56 -0600

    add bash script for making the plot making more streamlined

---
+++ analysis/2022.01_tdr/veff_breakdown/do_calculations.sh
@@ -0,0 +1,12 @@
+#declare -a dets=("0" "1" "2" "3")
+declare -a dets=("0" "2")
+declare -a modes=("deepshallow" "hybridshallow")
+
+for det in "${dets[@]}"
+do
+    for mode in "${modes[@]}"
+    do
+        # python calc_fractions.py --det $det --mode $mode # first, calculate the information
+        python plot_veffs.py --det $det --mode $mode # now, make plots and write to csv
+    done
+done

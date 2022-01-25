7d489f989b45c3c873d9b2566bb34c5a7e61c612
clark2668 <baclark@msu.edu>
2022-01-24 19:22:52 -0600

    upgrade the scripts for plotting of coincidence fraction correctly, also, allow us to plot the review array

--- analysis/2022.01_tdr/veff_breakdown/helper.py
+++ analysis/2022.01_tdr/veff_breakdown/helper.py
@@ -9,8 +9,9 @@ import pickle
 from multiprocessing import Pool
 from NuRadioReco.utilities import units
 from NuRadioMC.utilities.Veff import get_Veff_water_equivalent as gwe
+from NuRadioMC.utilities import cross_sections
 
-def tmp(filename, hybrid_list, shallow_list, hybrid_trigger, shallow_trigger):
+def tmp(filename, hybrid_list, shallow_list, deep_trigger, shallow_trigger):
     print("Filename {}".format(filename))
 
     fin = h5py.File(filename, "r")
@@ -56,8 +57,8 @@ def tmp(filename, hybrid_list, shallow_list, hybrid_trigger, shallow_trigger):
     for i, key in enumerate(tnames):
         tname_to_index[key] = i
     
-    if hybrid_trigger not in tname_to_index:
-        raise KeyError(f'Hybrid trigger ({hybrid_trigger}) is not in the hdf5 file list ({tnames})')
+    if deep_trigger not in tname_to_index:
+        raise KeyError(f'Hybrid trigger ({deep_trigger}) is not in the hdf5 file list ({tnames})')
     if shallow_trigger not in tname_to_index:
         raise KeyError(f'Shallow trigger ({shallow_trigger}) is not in the hdf5 file list ({tnames})')
 
@@ -65,20 +66,33 @@ def tmp(filename, hybrid_list, shallow_list, hybrid_trigger, shallow_trigger):
     # first, the hybrid-only part
     for key in fin.keys():
         if(key.startswith("station")):
-            deep_id = int(key.split('_')[1])
-            if deep_id not in hybrid_list:
+            hybrid_id = int(key.split('_')[1])
+            if hybrid_id not in hybrid_list:
                 continue
-            s_deep = fin[key]
-            if 'multiple_triggers_per_event' in s_deep:
-                triggers_deep = s_deep['multiple_triggers_per_event'][:, tname_to_index[hybrid_trigger]]
-                evs_triggers_deep = np.unique(np.array(s_deep['event_group_ids'])[triggers_deep])
+            s_hybrid = fin[key]
+            if 'multiple_triggers_per_event' in s_hybrid:
+
+                # this is the deep component of the hybrid array
+                hybrid_triggers_deep = s_hybrid['multiple_triggers_per_event'][:, tname_to_index[deep_trigger]]
+                evs_triggers_deep_hybrid = np.unique(np.array(s_hybrid['event_group_ids'])[hybrid_triggers_deep])
                 
-                for ev in evs_triggers_deep:
+                for ev in evs_triggers_deep_hybrid:
                     if ev not in event_information:
                         event_information[ev] = [weights_dict[ev], 0, 0]
-                        event_information[ev][1] = 1 # seen in SOME hybrid station
+                        event_information[ev][1] = 1 # seen in SOME deep component
                     elif ev in event_information:
-                        event_information[ev][1] +=1 # increment the number of hybrid stations where this is seen
+                        event_information[ev][1] +=1 # increment the number of deep components where this is seen
+
+                # now the shallow component of the hybrid array
+                hybrid_triggers_shallow = s_hybrid['multiple_triggers_per_event'][:, tname_to_index[shallow_trigger]]
+                evs_triggers_shallow_hybrid = np.unique(np.array(s_hybrid['event_group_ids'])[hybrid_triggers_shallow])
+                
+                for ev in evs_triggers_shallow_hybrid:
+                    if ev not in event_information:
+                        event_information[ev] = [weights_dict[ev], 0, 0]
+                        event_information[ev][2] = 1 # seen in SOME shallow component
+                    elif ev in event_information:
+                        event_information[ev][2] +=1 # increment the number of shallow components where this is seen
 
                 
     # second, the shallow-only part
@@ -100,29 +114,104 @@ def tmp(filename, hybrid_list, shallow_list, hybrid_trigger, shallow_trigger):
                         event_information[ev][2] +=1 # increment the number of hybrid stations where this is seen
 
     tot_weight = 0
-    hybrid_only_weight = 0
+    deep_only_weight = 0
     shallow_only_weight = 0
     dual_weight = 0
 
     for ev in event_information:
         weight = event_information[ev][0]
-        trig_hybr = event_information[ev][1]
+        trig_deep = event_information[ev][1]
         trig_shal = event_information[ev][2]
 
-        num_trig_total = trig_hybr + trig_shal
+        num_trig_total = trig_deep + trig_shal
         if num_trig_total > 0:
             tot_weight+=weight
         
-        if trig_hybr and trig_shal:
+        if trig_deep and trig_shal:
             dual_weight+=weight
-        elif trig_hybr and not trig_shal:
-            hybrid_only_weight += weight
-        elif trig_shal and not trig_hybr:
+        elif trig_deep and not trig_shal:
+            deep_only_weight += weight
+        elif trig_shal and not trig_deep:
             shallow_only_weight += weight
         
     summary['tot_weight'] = tot_weight
-    summary['hybrid_only_weight']  = hybrid_only_weight
+    summary['deep_only_weight']  = deep_only_weight
     summary['shallow_only_weight'] = shallow_only_weight
     summary['dual_weight'] = dual_weight
 
     return summary
+
+def get_review_array():
+    n_deep = 144*1
+    n_shallow = (169+144)*1
+    deep_det = 'pa_200m_2.00km'
+    deep_trigger = 'PA_4channel_100Hz'
+    shallow_det = 'surface_4LPDA_PA_15m_RNOG_300K_1.00km'
+    shallow_trigger = 'LPDA_2of4_100Hz'
+
+    flavors = ['e', 'mu', 'tau']
+    energies = np.zeros(9)
+    veffs = np.zeros((3,9))
+
+    data_i = np.genfromtxt(f'results/review_array/tabulated_veff_aeff_review_hybrid.csv', delimiter=',', skip_header=6, names=['logE', 'dveff', 'daff', 'sveff', 'saeff', 'overlap_frac', 'deep_only', 'shallow_only'])
+    energies = np.power(10.,data_i['logE'])
+    average_total_veff = np.zeros(9)
+    average_deep_veff = np.zeros(9)
+    average_shallow_veff = np.zeros(9)
+    average_dual_veff = np.zeros(9)
+    average_total_aeff = np.zeros(9)
+    average_deep_aeff = np.zeros(9)
+    average_shallow_aeff = np.zeros(9)
+    average_dual_aeff = np.zeros(9)
+
+    for iF, flavor in enumerate(flavors):
+        filename = f'results/review_array/overlap_{deep_det}_{deep_trigger}_{shallow_det}_{shallow_trigger}_{flavor}.pkl'
+        data = pickle.load(open(filename, 'br'))
+        for i, key in enumerate(data.keys()): # assume all have the same number of energies
+            veff_total = data[key]['total_veff']
+            veff_deep = data[key]['deep_only_veff']
+            veff_shallow = data[key]['shallow_only_veff']
+            veff_dual = data[key]['dual_veff']
+
+            lint = cross_sections.get_interaction_length(np.power(10., float(key)))
+            aeff_total = veff_total/lint
+            aeff_deep = veff_deep/lint
+            aeff_shallow = veff_shallow/lint
+            aeff_dual = veff_dual/lint
+
+            # convert to the right units (I could be more compact than this, but want to be explicit)
+            veff_total = veff_total * 4 * np.pi / units.km**3
+            veff_deep = veff_deep * 4 * np.pi / units.km**3
+            veff_shallow = veff_shallow * 4 * np.pi / units.km**3
+            veff_dual = veff_dual * 4 * np.pi / units.km**3
+
+            aeff_total = aeff_total * 4 * np.pi / units.km**2
+            aeff_deep = aeff_deep * 4 * np.pi / units.km**2
+            aeff_shallow = aeff_shallow * 4 * np.pi / units.km**2
+            aeff_dual = aeff_dual * 4 * np.pi / units.km**2
+
+            average_total_veff[i] += veff_total
+            average_deep_veff[i] += veff_deep
+            average_shallow_veff[i] += veff_shallow
+            average_dual_veff[i] += veff_dual
+
+            average_total_aeff[i] += aeff_total
+            average_deep_aeff[i] += aeff_deep
+            average_shallow_aeff[i] += aeff_shallow
+            average_dual_aeff[i] += aeff_dual
+
+    # turn into flavor average
+    average_total_veff/=3
+    average_deep_veff/=3
+    average_shallow_veff/=3
+    average_dual_veff/=3
+    average_total_aeff/=3
+    average_deep_aeff/=3
+    average_shallow_aeff/=3
+    average_dual_aeff/=3
+
+    return average_total_veff, average_deep_veff, \
+            average_shallow_veff, average_dual_veff, \
+            average_total_aeff, average_deep_aeff, \
+            average_shallow_aeff, average_dual_aeff
+
--- analysis/2022.01_tdr/veff_breakdown/plot_veffs.py
+++ analysis/2022.01_tdr/veff_breakdown/plot_veffs.py
@@ -9,6 +9,7 @@ import sys
 import pickle
 from NuRadioReco.utilities import units
 from NuRadioMC.utilities import cross_sections
+import helper
 
 detectors = [
     "baseline_array",
@@ -21,7 +22,7 @@ top = "/data/sim/Gen2/radio/2020/gen2-tdr-2021/simulation_output/secondaries_170
 detector = detectors[2]
 config = "config_ARZ2020_noise"
 detsim = "D01detector_sim"
-hybrid_trigger = 'PA_4channel_100Hz'
+deep_trigger = 'PA_4channel_100Hz'
 shallow_trigger = 'LPDA_2of4_100Hz'
 
 flavors = [
@@ -34,23 +35,23 @@ for flavor in flavors:
     result[flavor] = {
                     'E' : [],
                     'total_veff': [],
-                    'hybrid_veff': [],
+                    'deep_veff': [],
                     'shallow_veff': [],
                     'dual_veff': [],
                     'total_aeff': [],
-                    'hybrid_aeff': [],
+                    'deep_aeff': [],
                     'shallow_aeff': [],
                     'dual_aeff': []
                     }
     
-    pkl_file_name = os.path.join('results/overlap_' + detector + "_hybridtrig_" + hybrid_trigger + "_shallowtrig" + shallow_trigger + "_" + flavor + ".pkl")
+    pkl_file_name = os.path.join('results/overlap_' + detector + "_deeptrig_" + deep_trigger + "_shallowtrig" + shallow_trigger + "_" + flavor + ".pkl")
     with open(pkl_file_name, "rb") as fin:
         coincidences = pickle.load(fin)
     
     for lgE, value in coincidences.items():
         result[flavor]['E'].append(lgE)
         veff_total = value['total_veff']
-        veff_deep = value['hybrid_only_veff']
+        veff_deep = value['deep_only_veff']
         veff_shallow = value['shallow_only_veff']
         veff_dual = value['dual_veff']
         
@@ -72,11 +73,11 @@ for flavor in flavors:
         aeff_dual = aeff_dual * 4 * np.pi / units.km**2
 
         result[flavor]['total_veff'].append(veff_total)
-        result[flavor]['hybrid_veff'].append(veff_deep)
+        result[flavor]['deep_veff'].append(veff_deep)
         result[flavor]['shallow_veff'].append(veff_shallow)
         result[flavor]['dual_veff'].append(veff_dual)
         result[flavor]['total_aeff'].append(aeff_total)
-        result[flavor]['hybrid_aeff'].append(aeff_deep)
+        result[flavor]['deep_aeff'].append(aeff_deep)
         result[flavor]['shallow_aeff'].append(aeff_shallow)
         result[flavor]['dual_aeff'].append(aeff_dual)
 
@@ -92,21 +93,28 @@ average_dual_aeff = np.zeros(9)
 for iflavor, flavor in enumerate(flavors):
     for iE, energy in enumerate(energies):
         average_total_veff[iE]+=result[flavor]['total_veff'][iE]/3
-        average_deep_veff[iE]+=result[flavor]['hybrid_veff'][iE]/3
+        average_deep_veff[iE]+=result[flavor]['deep_veff'][iE]/3
         average_shallow_veff[iE]+=result[flavor]['shallow_veff'][iE]/3
         average_dual_veff[iE]+=result[flavor]['dual_veff'][iE]/3
         average_total_aeff[iE]+=result[flavor]['total_aeff'][iE]/3
-        average_deep_aeff[iE]+=result[flavor]['hybrid_aeff'][iE]/3
+        average_deep_aeff[iE]+=result[flavor]['deep_aeff'][iE]/3
         average_shallow_aeff[iE]+=result[flavor]['shallow_aeff'][iE]/3
         average_dual_aeff[iE]+=result[flavor]['dual_aeff'][iE]/3
+
+do_review = False
+if do_review:
+    average_total_veff, average_deep_veff, average_shallow_veff, average_dual_veff, average_total_aeff, average_deep_aeff, average_shallow_aeff, average_dual_aeff = helper.get_review_array()
+    detector = 'review array'
+    deep_trigger='review'
+    shallow_trigger='review'
 
 fraction_deep = average_deep_veff/average_total_veff
 fraction_shallow = average_shallow_veff/average_total_veff
 fraction_dual = average_dual_veff/average_total_veff
 
-output_csv = 'log10(energy [eV]), veff total [km^3 sr water equiv], veff hybrid only, veff shallow only, veff coincidence, '
-output_csv += 'aeff total [km^2 sr water equiv], aeff hybrid only, aeff shallow only, aeff coincidence, '
-output_csv += 'frac hybrid only, frac shallow only, frac coincidence'
+output_csv = 'log10(energy [eV]), veff total [km^3 sr water equiv], veff deep only, veff shallow only, veff coincidence, '
+output_csv += 'aeff total [km^2 sr water equiv], aeff deep only, aeff shallow only, aeff coincidence, '
+output_csv += 'frac deep only, frac shallow only, frac coincidence'
 output_csv += "\n"
 for iE, energy in enumerate(energies):
     output_csv += '{:.1f}, {:e}, {:e}, {:e}, {:e}, {:e}, {:e}, {:e}, {:e}, {:>.2f}, {:>.2f}, {:>.2f} \n'.format(
@@ -114,7 +122,7 @@ for iE, energy in enumerate(energies):
         average_total_veff[iE], average_deep_veff[iE], average_shallow_veff[iE], average_dual_veff[iE],
         average_total_aeff[iE], average_deep_aeff[iE], average_shallow_aeff[iE], average_dual_aeff[iE],
         fraction_deep[iE], fraction_shallow[iE], fraction_dual[iE])
-with open(f'results/coinc_{detector}_hybridtrig_{hybrid_trigger}_shallowtrig_{shallow_trigger}.csv', 'w') as fout:
+with open(f'results/coinc_{detector}_deeptrig_{deep_trigger}_shallowtrig_{shallow_trigger}.csv', 'w') as fout:
     fout.write(output_csv)
 
 fig, axs = plt.subplots(1, 2, figsize=(12,6))
@@ -125,9 +133,9 @@ xx = result[flavor]['E']
 fig.suptitle(f"{detector}")
 
 axs[0].plot(xx, average_deep_aeff+average_shallow_aeff+average_dual_aeff, styles[3], label='Sum')
-axs[0].plot(xx, average_deep_aeff, styles[0], label='Hybrid Only')
-axs[0].plot(xx, average_shallow_aeff, styles[1], label='Shallow Only')
-axs[0].plot(xx, average_dual_aeff, styles[2], label='Hybrid-Shallow Coincidence')
+axs[0].plot(xx, average_deep_aeff, styles[0], label='Deep Component')
+axs[0].plot(xx, average_shallow_aeff, styles[1], label='Shallow Component')
+axs[0].plot(xx, average_dual_aeff, styles[2], label='Deep-Shallow Coincidence')
 axs[0].set_yscale('log')
 axs[0].set_xlabel("log10(energy [eV])")
 axs[0].set_ylabel(r"[km$^2$ * str]")
@@ -136,14 +144,14 @@ axs[0].set_ylim([1E-5,1E2])
 axs[0].legend(loc='lower right')
 
 
-axs[1].plot(xx, fraction_deep, styles[0], label='Hybrid Only')
-axs[1].plot(xx, fraction_shallow, styles[1], label='Shallow Only')
-axs[1].plot(xx, fraction_dual, styles[2], label='Hybrid-Shallow Coincidence')
+axs[1].plot(xx, fraction_deep, styles[0], label='Deep Component')
+axs[1].plot(xx, fraction_shallow, styles[1], label='Shallow Component')
+axs[1].plot(xx, fraction_dual, styles[2], label='Deep-Shallow Coincidence')
 axs[1].plot(xx, fraction_deep+fraction_shallow+fraction_dual, styles[3], label='Sum')
 axs[1].set_ylim([0, 1.1])
 axs[1].set_xlabel("log10(energy [eV])")
 axs[1].set_ylabel("Fraction")
 axs[1].set_title("Fraction of the All-Sky Effective Area")
 fig.tight_layout(pad=2)
-outfilename = f'veff_fractions_{detector}_hybridtrig_{hybrid_trigger}_shallowtrig_{shallow_trigger}.png'
+outfilename = f'plots/veff_fractions_{detector}_deeptrig_{deep_trigger}_shallowtrig_{shallow_trigger}.png'
 fig.savefig(outfilename)

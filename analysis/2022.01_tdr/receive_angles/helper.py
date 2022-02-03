801c01bd4d2a1f314a3df9ef1e6dd0c857634e45
clark2668 <baclark@msu.edu>
2022-02-03 13:33:09 -0600

    update code to also show the focusing factors

--- analysis/2022.01_tdr/receive_angles/helper.py
+++ analysis/2022.01_tdr/receive_angles/helper.py
@@ -11,26 +11,25 @@ from multiprocessing import Pool
 def dig_around(filename, deep_trigger, shallow_trigger, hybrid_list, shallow_list):
 
     # hybrid_station_deep_channels = [4, 5, 6, 7]
-
     print("Filename {}".format(filename))
 
     fin = h5py.File(filename, "r")
 
-    ugids, uindex = np.unique(np.array(fin['event_group_ids']), 
-        return_index=True)
     the_keys = fin.keys()
 
     try:
         tnames = fin.attrs['trigger_names']
     except:
-        return None, None
+        return None, None, None, None
     
     tname_to_index = {}
     for i, key in enumerate(tnames):
         tname_to_index[key] = i
 
     all_thetas = []
+    all_ffs = []
     max_amp_thetas = []
+    max_amp_ffs = []
 
     for key in the_keys:
         if(key.startswith("station")):
@@ -46,21 +45,24 @@ def dig_around(filename, deep_trigger, shallow_trigger, hybrid_list, shallow_lis
             if 'multiple_triggers' in s:
                 trigger_mask = s['multiple_triggers'][:, tname_to_index[deep_trigger]]
                 shower_ids = s['shower_id']
+                # print(np.asarray(shower_ids))
                 rec_vec = s['receive_vectors']
+                focus_factors = s['focusing_factor']
                 amps = s['max_amp_shower_and_ray']
                 # print('Shower ids {}, Mask {}, Rec Vectors {}'.format(shower_ids, trigger_mask, rec_vec))
                 # print('{}, {}'.format(rec_vec, type))
 
-                # trig_showers = shower_ids[trigger_mask]
                 # the following slice grabs the last 8 antennas (4:)
                 # and the 2 solutions * 3 elements of the array
                 # without the 4:, this would have shape n_channels * n_solutions * 3
                 trig_rec_vec = rec_vec[trigger_mask, 4:] 
                 trig_amps = amps[trigger_mask, 4:]
+                trig_ff = focus_factors[trigger_mask, 4:]
                 
-                for shower_amps, shower_vecs in zip(trig_amps, trig_rec_vec):
+                for shower_amps, shower_vecs, ff in zip(trig_amps, trig_rec_vec, trig_ff):
                     temp_amps = np.asarray(shower_amps)
                     temp_rec_angs = np.asarray(shower_vecs)
+                    temp_ffs = np.asarray(ff)
                     max_element = np.nanmax(temp_amps)
                     index = np.where(temp_amps==max_element)
                     # print(temp_amps)
@@ -69,6 +71,9 @@ def dig_around(filename, deep_trigger, shallow_trigger, hybrid_list, shallow_lis
                     the_vec = temp_rec_angs[index][0]
                     temp_theta = np.arccos(the_vec[2])
                     max_amp_thetas.append(temp_theta)
+                    # max_amp_ffs
+                    the_ff = temp_ffs[index][0]
+                    max_amp_ffs.append(the_ff)
                     # print("The vec {}".format(the_vec))
                 
 
@@ -78,36 +83,18 @@ def dig_around(filename, deep_trigger, shallow_trigger, hybrid_list, shallow_lis
                     for vec in choosy_antenna:
                         the_theta = np.arccos(vec[2])
                         all_thetas.append(the_theta)
-                        # print(vec)
-                    # for ch in shower: # loop over channel
-                        # for vec in ch: # loop over solutions
-                            # print(vec)
-                            # the_theta = np.acos(vec[2])
-                            # thetas.append(the_theta)
+
+                for ff in trig_ff:
+                    choosy_antenna = ff[4]
+                    all_ffs.append(choosy_antenna[0]) # dir sol
+                    all_ffs.append(choosy_antenna[1]) # refr/refl sol
+                    # print("the ff {}".format(the_ff))
+
+                    # print("    *--*")
     
-
-                # trig_rec_vec_deep = trig_rec_vec[4,:]
-                # print(trig_rec_vec_deep)
-
-                # for l in trig_rec_vec:
-                #     for m in l:
-                #         for n in m:
-                #             print('    n: {}'.format(n))
-                    # print('    l: {}'.format(l))
-                # print('  Trig Showers {}'.format(trig_showers))
-                # print('  Rec Vecs {}'.format(trig_rec_vec))
-
-                # trig_showers = shower_ids[trigger_mask]
-                # print('Trig Showers {}'.format(trig_showers))
-
-                # with_trigs = rec_vec[trigger_mask, :]
-                # print("With trig {}".format(len(with_trigs)))
-                # print('Rec Vec {}'.format(rec_vec[trigger_mask, :]))
 
                 # print('----')
                 # print('\n\n')
 
-    return all_thetas, max_amp_thetas
-
-
+    return all_thetas, max_amp_thetas, all_ffs, max_amp_ffs
 
--- analysis/2022.01_tdr/receive_angles/quick_look_hdf5.py
+++ analysis/2022.01_tdr/receive_angles/quick_look_hdf5.py
@@ -1,6 +1,8 @@
+from re import A
 import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
+import matplotlib.colors as colors
 import h5py
 import glob
 import os
@@ -15,16 +17,19 @@ detector = 'hex_shallowheavy_array'
 hybrid_list = np.genfromtxt(f"station_lists/stations_{detector}_hybrid.csv")
 shallow_list = np.genfromtxt(f"station_lists/stations_{detector}_shallow.csv")
 
-# files = '/data/sim/Gen2/radio/2020/gen2-tdr-2021/simulation_output/secondaries_1700km2/step3/hex_shallowheavy_array/config_ARZ2020_noise/D01detector_sim/e/e_18.00eV*_-0.5_*'
+# files = '/data/sim/Gen2/radio/2020/gen2-tdr-2021/simulation_output/secondaries_1700km2/step3/hex_shallowheavy_array/config_ARZ2020_noise/D01detector_sim/e/e_18.00eV*_-0.1_*'
 files = '/data/sim/Gen2/radio/2020/gen2-tdr-2021/simulation_output/secondaries_1700km2/step3/hex_shallowheavy_array/config_ARZ2020_noise/D01detector_sim/e/e_18.00eV*.hdf5'
 
 # filename = 'e_20.00eV_0.0_0.1.part000000.hdf5'
 # filename = 'e_17.00eV_0.0_0.1.hdf5'
 filenames = sorted(glob.glob(files))
 all_thetas = []
+all_ffs = []
 max_amp_thetas = []
+max_amp_ffs = []
+deep_trigger = 'PA_4channel_100Hz'
 for f in filenames:
-    temp_all_thetas, temp_max_amp_thetas = helper.dig_around(f, 'PA_4channel_100Hz', 'LPDA_2of4_100Hz',
+    temp_all_thetas, temp_max_amp_thetas, temp_all_ffs, temp_max_amp_ffs = helper.dig_around(f, deep_trigger , 'LPDA_2of4_100Hz',
         hybrid_list, shallow_list
     )
     if temp_all_thetas is not None:
@@ -33,6 +38,12 @@ for f in filenames:
     if temp_max_amp_thetas is not None:
         for t in temp_max_amp_thetas:
             max_amp_thetas.append(t)
+    if temp_all_ffs is not None:
+        for t in temp_all_ffs:
+            all_ffs.append(t)
+    if temp_max_amp_ffs is not None:
+        for t in temp_max_amp_ffs:
+            max_amp_ffs.append(t)
 
 all_thetas = np.rad2deg(all_thetas)
 max_amp_thetas = np.rad2deg(max_amp_thetas)
@@ -53,4 +64,46 @@ ax.set_title('NuE, 1E18')
 ax.set_xlabel('Receive Angle (deg)')
 ax.set_ylabel('PDF')
 ax.legend()
-fig.savefig('for_dave.png')
+fig.savefig('for_dave_recang_{}.png'.format(deep_trigger))
+
+del fig, ax
+
+cmap=plt.cm.plasma
+fig = plt.figure(figsize=(14,6))
+ax = fig.add_subplot(121)
+ax2 = fig.add_subplot(122)
+bins = [np.linspace(0,180,180), np.linspace(0,2.2,220)]
+
+counts, xedges, yedges, im = ax.hist2d(
+    all_thetas,
+    all_ffs,
+    bins=bins,
+    cmap=cmap,
+    norm=colors.LogNorm(),
+    cmin=1
+)
+cbar = plt.colorbar(im, ax=ax)
+cbar.set_label('Counts')
+ax.set_title('NuE, 1E18')
+ax.set_xlabel('Receive Angle (deg)')
+ax.set_ylabel('Focusing Factor')
+ax.set_title('Ch8, All Showers, All Ray')
+
+
+counts, xedges, yedges, im = ax2.hist2d(
+    max_amp_thetas,
+    max_amp_ffs,
+    bins=bins,
+    cmap=cmap,
+    norm=colors.LogNorm(),
+    cmin=1
+)
+cbar2 = plt.colorbar(im, ax=ax2)
+cbar2.set_label('Counts')
+ax2.set_title('NuE, 1E18')
+ax2.set_xlabel('Receive Angle (deg)')
+ax2.set_ylabel('Focusing Factor')
+ax2.set_title('Max Amp Shower/Channel/Ray')
+
+plt.tight_layout()
+fig.savefig('for_dave_ffs_{}.png'.format(deep_trigger))
